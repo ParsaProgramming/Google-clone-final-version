e515dbbbc427cc2908bf0494c7df01e9e7f76b4d
Parsa <mohammadkazemi.parsa@gmail.com>
2023-01-17 10:43:52 +0330

    added settings menu

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+let settings_icon = document.querySelector(".setting-icon");
+let settings_popup = document.querySelector(".popup-settings");
+
+
+settings_icon.addEventListener("click", ()=>{
+    settings_popup.classList.add("show");
+})
