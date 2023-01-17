c3eb74b0693566406f327bd2c3ef4d707f27bf17
Parsa <mohammadkazemi.parsa@gmail.com>
2023-01-17 11:12:50 +0330

    close button added

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,12 @@
 let settings_icon = document.querySelector(".setting-icon");
 let settings_popup = document.querySelector(".popup-settings");
+let close_window_btn = document.querySelector(".final-buttons button.close");
 
 
 settings_icon.addEventListener("click", ()=>{
     settings_popup.classList.add("show");
-})
+});
+
+close_window_btn.addEventListener("click", ()=>{
+    settings_popup.classList.remove("show");
+});
