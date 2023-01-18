bae5b3958c2bf9ba42dcaf04f04b55540bfad018
Parsa <mohammadkazemi.parsa@gmail.com>
2023-01-18 16:16:22 +0330

    popup window completed

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,16 @@
 let settings_icon = document.querySelector(".setting-icon");
 let settings_popup = document.querySelector(".popup-settings");
-let close_window_btn = document.querySelector(".final-buttons button.close");
+let close_window_btn = document.querySelector(".popup-settings button");
+// changing Bg
+let ImageBgSource = document.querySelectorAll(".popup-settings .option .container .row .col img");
+// main Bg
+let DefaultImg = document.querySelector(".bg-place img");
 
+ImageBgSource.forEach(bg => {
+    bg.addEventListener("click", ()=>{
+        DefaultImg.setAttribute("src", bg.getAttribute("src"));
+    })
+});
 
 settings_icon.addEventListener("click", ()=>{
     settings_popup.classList.add("show");
